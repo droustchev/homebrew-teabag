28d394ee4e0edd41d29dace15d077ee6047c47b7
Dimitar Roustchev <dimitar@pagerduty.com>
2018-09-29 01:30:06 +0300

    Add more teagabs

--- teabag.rb
+++ teabag.rb
@@ -2,10 +2,11 @@ class Teabag < Formula
   desc ''
   homepage ''
   url 'https://github.com/droustchev/homebrew-teabag.git'
-  version '0.1'
+  version '0.2'
 
   def install
     puts "Teabag!"
+    bin.install ['package_files/teabag/teabag']
   end
 
 end
